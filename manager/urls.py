f45ae706a622277e15c2194c3e62b75f4128db3e
Roman Zhadan <21gehassig@gmail.com>
2023-11-27 15:26:50 +0200

    create app manager

---
+++ manager/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from manager.views import (
+    TaskList,
+    TaskCreate,
+    TaskUpdate,
+    TaskDelete,
+    TagList,
+    TagCreate,
+    TagUpdate,
+    TagDelete,
+    task_status_update,
+)
+
+urlpatterns = [
+    path("", TaskList.as_view(), name="task-list"),
+    path("task/create/", TaskCreate.as_view(), name="task-create"),
+    path("task/<int:pk>/update/", TaskUpdate.as_view(), name="task-update"),
+    path("task/<int:pk>/status-update/", task_status_update, name="task-status-update"),
+    path("task/<int:pk>/delete/", TaskDelete.as_view(), name="task-delete"),
+    path("tags/", TagList.as_view(), name="tag-list"),
+    path("tags/create/", TagCreate.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdate.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDelete.as_view(), name="tag-delete"),
+]
+
+
+app_name = "manager"
---
+++ manager/views.py
@@ -0,0 +1,65 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from manager.forms import TaskForm
+from manager.models import Task, Tag
+
+
+class TaskList(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
+    paginate_by = 10
+
+
+class TaskCreate(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TaskUpdate(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TaskDelete(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TagList(generic.ListView):
+    model = Tag
+    paginate_by = 5
+
+
+class TagCreate(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("manager:tag-list")
+
+
+class TagUpdate(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("manager:tag-list")
+
+
+class TagDelete(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("manager:tag-list")
+
+
+def task_status_update(request, pk: int):
+    task = Task.objects.get(pk=pk)
+
+    if task.done:
+        task.done = False
+    else:
+        task.done = True
+
+    task.save()
+
+    return HttpResponseRedirect(reverse_lazy("manager:task-list"))
